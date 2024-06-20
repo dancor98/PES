32dab6f15f183efc6f6a2953aa726250dca40ff9
Daniel Cordero <corderocalderondaniel@gmail.com>
2024-06-20 15:44:11 -0600

    Arreglo pantalla inicio

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -1,31 +1,28 @@
-<div class="contenedor-inicio"></div>
+<div class="contenedor-inicio">
+    <h1 class="titulo-inicio">Nuentra Historia</h1>
 
-<div class="contenido-principal">
-</div>
+    <div>
+        <p class="descripcion-inicio">✔ Nuestro negocio empezó en 2001 con una tienda llamada Bikestation.</p>
+        <p class="descripcion-inicio">✔ CCM Soluciones en Servicios comenzó en 2008, como distribuidor mayorista de
+            Specialized.</p>
+        <p class="descripcion-inicio">✔ A día de hoy, contamos con 8 tiendas en todo el país y un E-Commerce.</p>
+        <img class="imagen-linea" src="/build/img/linea_tiempo.png" alt="">
 
-<div class="contenedor-principal">
-    <div class="contenedor-principal__left">
-        <h1 class="contenedor-principal__left__titulo">Mision</h1>
-    </div>
-    <div class="contenedor-principal__right">
-        <p class="contenedor-principal__right__texto">Brindamos la mejor experiencia de este
-            mercado con conciencia individual e
-            integración grupal, caracterizados por
-            una gestión de liderazgo positivo y de
-            crecimiento continuo, que evidencia
-            pasión, respeto, perseverancia,
-            responsabilidad y compromiso.</p>
     </div>
 
-    <div class="contenedor-principal__left">
-        <h1 class="contenedor-principal__left__titulo">Vision</h1>
+
+    <h1 class="titulo-inicio titulo-inicio--2">Distribución CCM</h1>
+
+    <div class="columna">
+        <h2 class="subtitulo-inicio">Nuestras Tiendas</h2>
+        <p class="descripcion-inicio">✔ Contamos 5 puntos de venta en la Gran Área Metropolitana, que cubren más del 60%
+            de la población.</p>
+        <p class="descripcion-inicio">✔ Nuestras otras tiendas en las ciudades periféricas del país, que cubren el resto
+            de la población.</p>
+        <p class="descripcion-inicio">✔ contamos con una red de más de 50 tiendas en el resto del país para garantizar
+            el posicionamiento y la distribución.</p>
     </div>
-    <div class="contenedor-principal__right">
-        <p class="contenedor-principal__right__texto">
-            Ser el equipo líder del mercado,
-            excepcional, innovador, comprometido
-            con la proactividad y productividad, en
-            beneficio de accionistas, personas
-            internas, externas y comunidad.</p>
+    <div class="columna">
+        <img class="imagen-mapa" src="/build/img/mapa.png" alt="">
     </div>
 </div>
