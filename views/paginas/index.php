f3e192dc01df703f1cc4f2ef24adeedfd70253ff
Daniel Cordero <corderocalderondaniel@gmail.com>
2024-06-20 21:29:55 -0600

    .

--- views/paginas/index.php
+++ views/paginas/index.php
@@ -1,28 +1,21 @@
+<div class="hero">
+</div>
+
+<div class="contenedor-inicio-1">
+    <p class="texto-ccm">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non laborum architecto aperiam, adipisci voluptas
+        tempore iusto? Optio quasi facilis pariatur cumque ducimus alias magnam dicta odit ipsum, fugiat molestias
+        repudiandae?
+    </p>
+    <p class="firma-ceo">- CEO Carlos Campos M.</p>
+</div>
 <div class="contenedor-inicio">
-    <h1 class="titulo-inicio">Nuentra Historia</h1>
 
-    <div>
-        <p class="descripcion-inicio">✔ Nuestro negocio empezó en 2001 con una tienda llamada Bikestation.</p>
-        <p class="descripcion-inicio">✔ CCM Soluciones en Servicios comenzó en 2008, como distribuidor mayorista de
-            Specialized.</p>
-        <p class="descripcion-inicio">✔ A día de hoy, contamos con 8 tiendas en todo el país y un E-Commerce.</p>
-        <img class="imagen-linea" src="/build/img/linea_tiempo.png" alt="">
-
-    </div>
-
-
-    <h1 class="titulo-inicio titulo-inicio--2">Distribución CCM</h1>
+    <h1 class="titulo-inicio titulo-inicio">Distribución CCM</h1>
 
     <div class="columna">
-        <h2 class="subtitulo-inicio">Nuestras Tiendas</h2>
-        <p class="descripcion-inicio">✔ Contamos 5 puntos de venta en la Gran Área Metropolitana, que cubren más del 60%
-            de la población.</p>
-        <p class="descripcion-inicio">✔ Nuestras otras tiendas en las ciudades periféricas del país, que cubren el resto
-            de la población.</p>
-        <p class="descripcion-inicio">✔ contamos con una red de más de 50 tiendas en el resto del país para garantizar
-            el posicionamiento y la distribución.</p>
+
     </div>
     <div class="columna">
-        <img class="imagen-mapa" src="/build/img/mapa.png" alt="">
     </div>
 </div>
--- views/templates/header.php
+++ views/templates/header.php
@@ -1,7 +1,7 @@
-<div class="menu">
+<div class="menu menu-top">
     <nav class="navbar navbar-inicio navbar-expand-lg ">
         <div class="container-fluid">
-            <a class="navbar-brand" href="#">
+            <a class="navbar-brand" href="/">
                 <img class="navbar-inicio-logo" src="/build/img/logo.png" alt="">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -9,15 +9,13 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav ms-auto">
-                    <a class="nav-link nav-link-menu" href="#">INICIO</a>
+                    <a class="nav-link nav-link-menu" href="/">INICIO</a>
                     <a class="nav-link nav-link-menu" href="#">NOSOTROS</a>
-                    <a class="nav-link nav-link-menu" href="#">EMPLEO</a>
+                    <a class="nav-link nav-link-menu" href="#">PRODUCTO Y SERVICIOS</a>
+                    <a class="nav-link nav-link-menu" href="#">LOCALIDADES</a>
+                    <a class="nav-link nav-link-menu" href="#">CARRERAS</a>
                 </div>
             </div>
         </div>
     </nav>
-
-    <div class="texto-inicio">
-        <p>Distribuidor Oficial de Specialized Costa Rica.</p>
-    </div>
 </div>
