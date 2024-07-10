f2ca59fd1cab206f997df06664df3607c0b64051
Daniel Cordero <corderocalderondaniel@gmail.com>
2024-07-10 15:16:56 -0600

    c

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,4 @@
 
-
-
-
-
 document.addEventListener('DOMContentLoaded', function() {
     var form = document.getElementById('FormularioInterno');
     var submitButton = document.getElementById('botonSubmit');
@@ -13,4 +9,46 @@ document.addEventListener('DOMContentLoaded', function() {
         submitButton.value =
         'Cargando...'; // Opcional: Cambia el texto del botón para indicar que está en proceso.
     });
+});
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    function getQueryParams() {
+        let params = {};
+        window.location.search.substring(1).split('&').forEach(function(param) {
+            let parts = param.split('=');
+            params[parts[0]] = decodeURIComponent(parts[1]);
+        });
+        return params;
+    }
+
+    let params = getQueryParams();
+    if (params.estado === 'exito') {
+        var myModal = new bootstrap.Modal(document.getElementById('exito'));
+        myModal.show();
+    } else if (params.estado === 'error') {
+        var myModal = new bootstrap.Modal(document.getElementById('error'));
+        myModal.show();
+    }
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    function getQueryParams() {
+        let params = {};
+        window.location.search.substring(1).split('&').forEach(function(param) {
+            let parts = param.split('=');
+            params[parts[0]] = decodeURIComponent(parts[1]);
+        });
+        return params;
+    }
+
+    let params = getQueryParams();
+    if (params.estado === 'exito') {
+        var myModal = new bootstrap.Modal(document.getElementById('exito'));
+        myModal.show();
+    } else if (params.estado === 'error') {
+        var myModal = new bootstrap.Modal(document.getElementById('error'));
+        myModal.show();
+    }
 });
