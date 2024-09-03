3930f17fe76dd66a787addf82ae8706f6f2c61cd
Daniel Cordero <corderocalderondaniel@gmail.com>
2024-09-03 16:35:35 -0600

    Correccion Vacaciones

--- src/js/app.js
+++ src/js/app.js
@@ -126,7 +126,118 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
-//vacaciones
+// //vacaciones
+// document.addEventListener("DOMContentLoaded", function () {
+//   var hoy = new Date();
+//   hoy.setDate(hoy.getDate() + 1); // Sumamos un día a la fecha actual para obtener mañana
+//   var manana = hoy.toISOString().split("T")[0];
+
+//   var desde = document.getElementById("desde");
+//   var hasta = document.getElementById("hasta");
+//   var cantidad = document.getElementById("cantidad");
+//   var cantidad_dias = document.getElementById("cantidad_dias");
+//   var botonSubmit = document.getElementById("botonSubmit");
+
+//   // Establecemos el valor mínimo para el campo "desde" como mañana
+//   if (desde) {
+//     desde.setAttribute("min", manana);
+
+//     desde.addEventListener("change", function () {
+//       validarFecha(desde);
+//       // Establecemos el valor mínimo para el campo "hasta" como el valor seleccionado en "desde"
+//       if (hasta) {
+//         hasta.setAttribute("min", desde.value);
+//       }
+//       actualizarCantidadDias();
+//     });
+//   }
+
+//   if (hasta) {
+//     hasta.addEventListener("change", function () {
+//       validarFecha(hasta);
+//       actualizarCantidadDias();
+//     });
+//   }
+
+//   if (botonSubmit) {
+//     botonSubmit.addEventListener("click", function (event) {
+//       // Validamos que las fechas sean correctas
+//       if (desde && desde.value < manana) {
+//         alert("La fecha 'Desde' no puede ser menor a mañana.");
+//         event.preventDefault();
+//         return false;
+//       }
+
+//       if (hasta && hasta.value < desde.value) {
+//         alert("La fecha 'Hasta' no puede ser menor a la fecha 'Desde'.");
+//         event.preventDefault();
+//         return false;
+//       }
+//     });
+//   }
+
+//   function validarFecha(campo) {
+//     var fechaSeleccionada = new Date(campo.value);
+//     var diaSemana = fechaSeleccionada.getDay();
+
+//     // Si el día seleccionado es sábado (5) o domingo (6)
+//     if (diaSemana === 5 || diaSemana === 6) {
+//       alert("No se pueden seleccionar sábados ni domingos.");
+//       // Restablece el campo a vacío
+//       campo.value = "";
+//     }
+//   }
+
+//   function actualizarCantidadDias() {
+//     if (desde && hasta && desde.value && hasta.value) {
+//       var fechaDesde = new Date(desde.value);
+//       var fechaHasta = new Date(hasta.value);
+
+//       // Asegúrate de que la fechaHasta incluya el final del día
+//       fechaHasta.setHours(23, 59, 59, 999);
+
+//       if (fechaHasta >= fechaDesde) {
+//         var diferencia = 0;
+//         var fechaTemp = new Date(fechaDesde);
+
+//         // Iterar a través de cada día en el rango de fechas
+//         while (fechaTemp <= fechaHasta) {
+//           var diaSemana = fechaTemp.getDay();
+//           if (diaSemana !== 5 && diaSemana !== 6) {
+//             // Excluye sábados (5) y domingos (6)
+//             diferencia++;
+//           }
+//           fechaTemp.setDate(fechaTemp.getDate() + 1);
+//         }
+
+//         if (cantidad) {
+//           cantidad.value = diferencia;
+//         }
+
+//         if (cantidad_dias) {
+//           cantidad_dias.value = diferencia;
+//         }
+//       } else {
+//         if (cantidad) {
+//           cantidad.value = "";
+//         }
+
+//         if (cantidad_dias) {
+//           cantidad_dias.value = "";
+//         }
+//       }
+//     } else {
+//       if (cantidad) {
+//         cantidad.value = "";
+//       }
+
+//       if (cantidad_dias) {
+//         cantidad_dias.value = "";
+//       }
+//     }
+//   }
+// });
+
 //vacaciones
 document.addEventListener("DOMContentLoaded", function () {
   var hoy = new Date();
@@ -138,6 +249,7 @@ document.addEventListener("DOMContentLoaded", function () {
   var cantidad = document.getElementById("cantidad");
   var cantidad_dias = document.getElementById("cantidad_dias");
   var botonSubmit = document.getElementById("botonSubmit");
+  var colaboradorId = document.getElementById("colaborador_id").value;
 
   // Establecemos el valor mínimo para el campo "desde" como mañana
   if (desde) {
@@ -181,9 +293,15 @@ document.addEventListener("DOMContentLoaded", function () {
     var fechaSeleccionada = new Date(campo.value);
     var diaSemana = fechaSeleccionada.getDay();
 
-    // Si el día seleccionado es sábado (6) o domingo (0)
-    if (diaSemana === 5 || diaSemana === 6) {
+    // Si el colaborador_id es 7, excluye sábados (5) y domingos (6)
+    if (colaboradorId == 7 && (diaSemana === 5 || diaSemana === 6)) {
       alert("No se pueden seleccionar sábados ni domingos.");
+      // Restablece el campo a vacío
+      campo.value = "";
+    }
+    // Si es otro colaborador, solo excluye domingos (6)
+    else if (diaSemana === 6) {
+      alert("No se pueden seleccionar domingos.");
       // Restablece el campo a vacío
       campo.value = "";
     }
@@ -204,9 +322,16 @@ document.addEventListener("DOMContentLoaded", function () {
         // Iterar a través de cada día en el rango de fechas
         while (fechaTemp <= fechaHasta) {
           var diaSemana = fechaTemp.getDay();
-          if (diaSemana !== 5 && diaSemana !== 6) {
-            // Excluye sábados (6) y domingos (0)
-            diferencia++;
+          // Si colaborador_id es 2, excluye sábados (5) y domingos (6)
+          if (colaboradorId == 2) {
+            if (diaSemana !== 5 && diaSemana !== 6) {
+              diferencia++;
+            }
+          } else {
+            // Excluye solo domingos (6) para otros colaboradores
+            if (diaSemana !== 6) {
+              diferencia++;
+            }
           }
           fechaTemp.setDate(fechaTemp.getDate() + 1);
         }
